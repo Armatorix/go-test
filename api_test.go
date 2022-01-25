e54d74ee00a7f9ba3f18c10f60cbc525f881b8da
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2022-01-26 00:20:30 +0100

    testify for tests

--- api_test.go
+++ api_test.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 var (
@@ -16,59 +18,35 @@ func runRequest(t *testing.T, srv http.Handler, r *http.Request) (content []*Con
 	response := httptest.NewRecorder()
 	srv.ServeHTTP(response, r)
 
-	if response.Code != 200 {
-		t.Fatalf("Response code is %d, want 200", response.Code)
-		return
-	}
+	require.Equal(t, http.StatusOK, response.Code)
 
 	decoder := json.NewDecoder(response.Body)
 	err := decoder.Decode(&content)
-	if err != nil {
-		t.Fatalf("couldn't decode Response json: %v", err)
-	}
+	require.NoError(t, err)
 
 	return content
 }
 
 func TestResponseCount(t *testing.T) {
 	content := runRequest(t, app, SimpleContentRequest)
-
-	if len(content) != 5 {
-		t.Fatalf("Got %d items back, want 5", len(content))
-	}
+	require.Len(t, content, 5)
 }
 
 func TestResponseOrder(t *testing.T) {
 	content := runRequest(t, app, SimpleContentRequest)
-
-	if len(content) != 5 {
-		t.Fatalf("Got %d items back, want 5", len(content))
-	}
+	require.Len(t, content, 5)
 
 	for i, item := range content {
-		if Provider(item.Source) != DefaultConfig[i%len(DefaultConfig)].Type {
-			t.Errorf(
-				"Position %d: Got Provider %v instead of Provider %v",
-				i, item.Source, DefaultConfig[i].Type,
-			)
-		}
+		require.Equal(t, DefaultConfig[i%len(DefaultConfig)].Type, item.Source)
 	}
 }
 
 func TestOffsetResponseOrder(t *testing.T) {
 	content := runRequest(t, app, OffsetContentRequest)
-
-	if len(content) != 5 {
-		t.Fatalf("Got %d items back, want 5", len(content))
-	}
+	require.Len(t, content, 5)
 
 	for j, item := range content {
 		i := j + 5
-		if Provider(item.Source) != DefaultConfig[i%len(DefaultConfig)].Type {
-			t.Errorf(
-				"Position %d: Got Provider %v instead of Provider %v",
-				i, item.Source, DefaultConfig[i].Type,
-			)
-		}
+		require.Equal(t, DefaultConfig[i%len(DefaultConfig)].Type, item.Source)
 	}
 }
